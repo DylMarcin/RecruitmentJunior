433573fb704d5754553cd6764d9c3b2d5fabf67c
DylMarcin <marcin.dyl@outlook.com>
2022-01-10 12:21:10 +0100

    task3.py final optimization

--- task3.py
+++ task3.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timedelta
 from functools import lru_cache
 from typing import Dict, List, Optional
-
 from requests import request
 
 DOMAIN = "https://recruitment.developers.emako.pl"
-
 
 class Connector:
     @lru_cache
@@ -17,29 +15,44 @@ class Connector:
             "Accept": "application/json",
         }
 
+    def append_pagination_data(self, data: dict, page:int) -> None:
+        if page:
+            data['pagination'] = {'index' : page}
+
+    def request_size_error(self, method:str):
+        return {"Error": f"{method} request processes a maximum of 20 elements"}
+
     def request(self, method: str, path: str, data: dict = {}) -> dict:
         return request(
             method, f"{DOMAIN}/{path}", json=data, headers=self.headers()
         ).json()
 
-    def get_products(self, ids: Optional[List[int]] = None) -> List[dict]:
-        return self.request("GET", "products", {"ids": ids})["result"]
+    def get_products(self, ids: Optional[List[int]] = None, page: int = None) -> List[dict]:
+        data = {"ids": ids}
+        self.append_pagination_data(data, page)
+        return self.request("GET", "products", data)["result"]
 
-    def get_all_products_summary(self) -> List[dict]:
-        return self.request("GET", "products", {"detailed": False})["result"]
+    def get_all_products_summary(self, page: int = None) -> List[dict]:
+        data = {"detailed": False}
+        self.append_pagination_data(data, page)
+        return self.request("GET", "products", data)["result"]
 
-    def get_new_products(self, newer_than: Optional[datetime] = None) -> List[dict]:
+    def get_new_products(self, newer_than: Optional[datetime] = None, page: int = None) -> List[dict]:
         if newer_than is None:
             newer_than = datetime.now() - timedelta(days=5)
-        return self.request(
-            "GET", "products", {"created_at": {"start": newer_than.isoformat()}}
-        )["result"]
+        data = {"created_at": {"start": newer_than.isoformat()}}
+        self.append_pagination_data(data, page)
+        return self.request("GET", "products", data)["result"]
 
     def add_products(self, products: List[dict]):
-        return self.request("POST", "products", {"products": products})["result"]
+        if len(products) <= 20:
+            return self.request("POST", "products", {"products": products})["result"]
+        return self.request_size_error("POST")
 
     def update_stocks(self, stocks: Dict[int, list]):
         current_data = self.get_products(list(stocks))
-        for product_entry in current_data:
-            product_entry["details"]["supply"] = stocks[product_entry["id"]]
-        return self.request("PUT", "products", {"products": current_data})
+        if len(current_data) <= 20:
+            for product_entry in current_data:
+                product_entry["details"]["supply"] = stocks[product_entry["id"]]
+            return self.request("PUT", "products", {"products": current_data})
+        return self.request_size_error("PUT")
